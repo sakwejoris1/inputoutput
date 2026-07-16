117810d85563083eb952728aee570ee4dc8e05ae
sakwejoris1 <sakwejoris@gmail.com>
2026-07-16 14:08:28 +0100

    chapter3

--- src/Account1.java
+++ src/Account1.java
@@ -1,16 +1,16 @@
 // Fig. 3.8: Account.java
 // Account class with a double instance variable balance and a constructor
 // and deposit method that perform validation.
-public class Account
+public class Account1
 {
     private String name; // instance variable
     private double balance; // instance variable
     // Account constructor that receives two parameters
-    public Account(String name, double balance)
+    public Account1(String name, double balance)
     {
         this.name = name; // assign name to instance variable name
-// validate that the balance is greater than 0.0; if it's not,
-// instance variable balance keeps its default initial value of 0.0
+        // validate that the balance is greater than 0.0; if it's not,
+        // instance variable balance keeps its default initial value of 0.0
         if (balance > 0.0) // if the balance is valid
             this.balance = balance; // assign it to instance variable balance
     }
---
+++ src/checkboard.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class checkboard {
+    public static void main(String[] args)
+    {
+        int i;
+        for (i = 0; i < 50; i++) {
+            System.out.print("*\n");
+            System.out.print("*\t");
+        }
+    }
+}
